7bd10a516970b21085a54023c338b6f5783b8510
Maira <maira212.a@gmail.com>
2025-08-10 16:33:24 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
+  let who = ['The teacher','My neighbor','The firefighter','My best friend', 'My dog'];
+  let action = ['kicked','painted','ate','lose','threw'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['after the meeting.','when the phone rang.','while I was cooking.','during the class.'];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)]
+  let randomAction = action[Math.floor(Math.random() * action.length)]
+  let randomWhat = what[Math.floor(Math.random() * what.length)]
+  let randomWhen = when[Math.floor(Math.random() * when.length)]
+
+  let newExcuse = (randomWho +' '+ randomAction +' '+ randomWhat +' '+ randomWhen);
+
+  document.getElementById("excuse").innerHTML=(newExcuse);
+  
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(newExcuse);
 };
