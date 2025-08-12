762c67a2c116bd24b5523142ac524f892693999a
Maira <maira212.a@gmail.com>
2025-08-12 16:46:49 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,23 +1,26 @@
 import "bootstrap";
 import "./style.css";
 
-
-
 window.onload = function () {
-  let who = ['The teacher','My neighbor','The firefighter','My best friend', 'My dog'];
-  let action = ['kicked','painted','ate','lose','threw'];
+  let who = ['The teacher', 'My neighbor', 'The firefighter', 'My best friend', 'My dog'];
+  let action = ['kicked', 'painted', 'ate', 'lose', 'threw'];
   let what = ['my homework', 'my phone', 'the car'];
-  let when = ['after the meeting.','when the phone rang.','while I was cooking.','during the class.'];
+  let when = ['after the meeting.', 'when the phone rang.', 'while I was cooking.', 'during the class.'];
 
-  let randomWho = who[Math.floor(Math.random() * who.length)]
-  let randomAction = action[Math.floor(Math.random() * action.length)]
-  let randomWhat = what[Math.floor(Math.random() * what.length)]
-  let randomWhen = when[Math.floor(Math.random() * when.length)]
 
-  let newExcuse = (randomWho +' '+ randomAction +' '+ randomWhat +' '+ randomWhen);
+  function randomExcuse() {
+    const randomWho = who[Math.floor(Math.random() * who.length)]
+    const randomAction = action[Math.floor(Math.random() * action.length)]
+    const randomWhat = what[Math.floor(Math.random() * what.length)]
+    const randomWhen = when[Math.floor(Math.random() * when.length)]
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+  }
 
-  document.getElementById("excuse").innerHTML=(newExcuse);
-  
-  //write your code here
-  console.log(newExcuse);
-};
+
+  document.getElementById("excuse").innerHTML = (randomExcuse());
+  console.log(randomExcuse());
+
+}
+
+
+
